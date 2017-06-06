761385c489b398a573f6729c1be7c73c2918b345
root <root@localhost.localdomain>
2017-06-06 23:01:40 +0000

    ipadd and master

--- chef-repo/cookbooks/ipadd/metadata.rb
+++ chef-repo/cookbooks/ipadd/metadata.rb
@@ -1,10 +1,8 @@
-name             'master'
+name             'ipadd'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
-description      'Installs/Configures master'
+description      'Installs/Configures ipadd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends "update"
-depends "ntp"
-depends "etcd"
--- chef-repo/cookbooks/master/recipes/default.rb
+++ chef-repo/cookbooks/master/recipes/default.rb
@@ -7,6 +7,40 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe 'update'
+include_recipe 'ntp'
+include_recipe 'etcd'
+
 package "kubernetes" do
-	action :install
+        action :install
+end
+
+package "cockpit" do
+        action :install
+end
+
+package "cockpit-docker" do
+        action :install
+end
+
+package "cockpit-kubernetes" do
+        action :install
+end
+service "kube-apiserver" do
+
+        action [ :enable, :start ]
+end
+
+service "kube-scheduler" do
+
+        action [ :enable, :start ]
+end
+
+service "kube-controller-manager" do
+
+        action [ :enable, :start ]
+end
+
+service "cockpit" do
+
+        action [ :enable, :start ]
 end
