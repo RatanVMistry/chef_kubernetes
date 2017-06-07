761f93728e1586440c00cb21f95f1accc0a8e245
root <root@localhost.localdomain>
2017-06-07 22:51:17 +0000

    master update

--- chef-repo/cookbooks/master/recipes/default.rb
+++ chef-repo/cookbooks/master/recipes/default.rb
@@ -25,22 +25,45 @@ end
 package "cockpit-kubernetes" do
         action :install
 end
+template "/etc/etcd/etcd.conf" do
+	source "etcd.conf.erb"
+	mode "0644"
+	
+	notifies :restart, "service[etcd]"
+end
+
+template "/etc/kubernetes/apiserver" do
+	source "apiserver.erb"
+	mode "0644"
+	
+	notifies :restart, "service[kube-apiserver]"
+end
+
 service "kube-apiserver" do
 
-        action [ :enable, :start ]
+        action [ :enable, :restart ]
 end
 
 service "kube-scheduler" do
 
-        action [ :enable, :start ]
+        action [ :enable, :restart ]
 end
 
 service "kube-controller-manager" do
 
-        action [ :enable, :start ]
+        action [ :enable, :restart ]
 end
 
 service "cockpit" do
 
         action [ :enable, :start ]
+end
+
+bash 'etcdctl' do
+	user 'root'
+	code <<-EOH
+	if ! (etcdctl get /atomic.io/network/config) ; then
+		etcdctl mk /atomic.io/network/config '{"Network":"172.17.0.0/16"}';
+	fi	
+	EOH
 end
